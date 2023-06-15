948ed9fbe241891752762536baa6d1efc8ce5e0a
rishihara <pechuelii33@gmail.com>
2023-06-15 19:41:45 +0900

    feat:query parameter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Union
 from pydantic import BaseModel
 
 # create a FastAPI "instance"
@@ -30,6 +31,11 @@ async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
 @app.get("/items/types/{item_type}")
 async def get_item_type(item_type: ItemType):
     if item_type is ItemType.gold:
@@ -37,6 +43,17 @@ async def get_item_type(item_type: ItemType):
     return {"item_name": "other", "item_type": item_type}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/items/actions/")
+# Parameters for which no default value is set are mandatory.
+# Parameters with default values are optional.
+# None is null.
+async def read_item_action(
+    action: str, isReq: bool = False, req: Union[str, None] = None
+):
+    if isReq == True:
+        return {
+            "action": action,
+            "isReq": isReq,
+            "req": req,
+        }
+    return {"action": action}
